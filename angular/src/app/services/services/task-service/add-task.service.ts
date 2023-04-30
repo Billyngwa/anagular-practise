7291f569dce8b938cb307334b0d50371f3a130a0
Billyngwa <billytatangwa5@gmail.com>
2023-04-30 09:02:03 +0100

    User session updated with task created by user

--- angular/src/app/services/services/task-service/add-task.service.ts
+++ angular/src/app/services/services/task-service/add-task.service.ts
@@ -4,6 +4,7 @@ import { Itask } from 'src/app/interfaces/task.interface';
 import { UsersessionService } from '../../sessionstore/usersession.service';
 import { UserService } from '../../userService/user.service';
 import { findIndex } from 'rxjs';
+import { SessionstorageService } from '../../sessionstore/sessionstorage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,7 @@ export class AddTaskService {
     userId: "",
     id: ""
   }
-  constructor(private localStore: DatabaseService, private userStore: UsersessionService, private userService: UserService) { }
+  constructor(private localStore: DatabaseService, private userStore: UsersessionService, private userService: UserService,private sessionstore:SessionstorageService) { }
   addTask(task: Itask) {
     task["userId"] = this.userStore.getsession()?.id; //Here i am setting the userId of the person who made the task and assigning it to the task userId value 
     //  let arrLength: number = usersInLocalStorage.length;
@@ -40,46 +41,72 @@ export class AddTaskService {
     const gettaskFromLocalStorage = this.localStore.get("Task");
     console.log(gettaskFromLocalStorage);
 
-    let taskFromLocalStorage = gettaskFromLocalStorage.data;// Here taskFromLocalStorage is ann object
+    let taskFromLocalStorage = gettaskFromLocalStorage.data;// Here taskFromLocalStorage is an object
     console.log(taskFromLocalStorage);
 
     let taskid = taskFromLocalStorage["userId"];
     console.log(taskid);
+    //At this point here we have gotten the task object from local storage to our fingertips as taskFromLocalStorage .
 
-    const getuserfromLocalstorage = this.localStore.get("Users");
-    console.log(getuserfromLocalstorage);
 
-    let userfromLocalStorage: object[] = [];
-    if (getuserfromLocalstorage.status === true) {
+    //here i am getting users from sessionstorage;
+    const getuserfromsessionstorage = this.sessionstore.get("loggedUser");
+    console.log(getuserfromsessionstorage);
+    let loggedObject//at this point we have the user from session storage
+    let usersTask;
 
-      userfromLocalStorage = getuserfromLocalstorage.data;
+    // let userfromLocalStorage: object[] = [];
+    if (getuserfromsessionstorage.status === true) {
+      console.log(getuserfromsessionstorage.data);
+      loggedObject=getuserfromsessionstorage.data;
+       usersTask = loggedObject["userTasks"];
+
+      // userfromLocalStorage = getuserfromLocalstorage.data;
     }
-    console.log(userfromLocalStorage);
+    console.log(usersTask);
+    console.log(task);
+    let updatedsession;
 
-    for (const aUser of userfromLocalStorage) {
-      if (aUser["id" as keyof object] === taskid) {
-        alert("User ids are same");
-        let userIndex = userfromLocalStorage.indexOf(aUser);
-        console.log(userIndex);
-
-        //at this point, we have both ids having same value. we need to now carry out our search and assign;
-        let taskKeyValue: object[] = aUser["userTasks" as keyof object]; // the return value here is an object of type Itask;
-        console.log(typeof taskKeyValue);
-        console.log(taskKeyValue);
-
-
-
-
-        console.log(`SUCCESS`);
-
-        return;
-      } else {
-        alert(`ERROR!! Ids do not match`);
-        alert(`Task was not save` + " " + `we are sorry An ERROR occured`);
-        return;
-      }
-      return;
+    if(taskid === loggedObject["id"]){
+      alert(`user ids match Id : ${loggedObject["id"]}`);
+      usersTask = task;
+      console.log(usersTask);
+      loggedObject["userTasks"]= usersTask;
+     updatedsession = this.sessionstore.set("loggedUser",loggedObject);
+      
     }
+    console.log(updatedsession);
+    
+
+
+    
+    //Now i need to check 
+    // console.log(userfromLocalStorage);// At this point i have all users from localstorage as an array of Objects
+
+    // for (const aUser of userfromLocalStorage) {
+    //   if (aUser["id" as keyof object] === taskid) {
+    //     alert("User ids are same");
+    //     let userIndex = userfromLocalStorage.indexOf(aUser);
+    //     console.log(userIndex);
+
+    //     //at this point, we have both ids having same value. we need to now carry out our search and assign;
+    //     let taskKeyValue: object[] = aUser["userTasks" as keyof object]; // the return value here is an object of type Itask;
+    //     console.log(typeof taskKeyValue);
+    //     console.log(taskKeyValue);
+
+
+
+
+    //     console.log(`SUCCESS`);
+
+    //     return;
+    //   } else {
+    //     alert(`ERROR!! Ids do not match`);
+    //     alert(`Task was not save` + " " + `we are sorry An ERROR occured`);
+    //     return;
+    //   }
+    //   return;
+    // }
 
     return someValue;
   }
